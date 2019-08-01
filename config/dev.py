8760a0fd8d5ce6ceb7139688c3ab51375a441db5
cyrilkhan <lyrickhan99@gmail.com>
2019-08-01 15:31:59 -0400

    initial commit

--- config/dev.py
+++ config/dev.py
@@ -1,4 +1,4 @@
 DEBUG = True
-SECRET_KEY = 'PIANO@230jap'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:PIANO@230jap@localhost/depths_char_sheet'
+SECRET_KEY = 'KotenlbaandChyPeimel'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:KotenlbaandChyPeimel@localhost/depths_char_sheet'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
